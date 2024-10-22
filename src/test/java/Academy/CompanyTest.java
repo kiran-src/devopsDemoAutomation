511519a0a55a34bc8701a1584408b77932eafa23
Kiran Setty <68366418+kiran-src@users.noreply.github.com>
2024-10-22 15:05:58 +0200

    Bug fix and update tests

---
+++ src/test/java/Academy/CompanyTest.java
@@ -0,0 +1,28 @@
+package Academy;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import variables.Constants;
+
+
+public class CompanyTest {
+
+	
+	@Test
+	public void getData() throws InterruptedException {
+		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get(Constants.USER_BASE_URL);
+		String text =driver.findElement(By.cssSelector("h2")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase(Constants.SUBHEADER_TEXT));
+		driver.close();
+	
+		
+		
+	}
+}
---
+++ src/test/java/Academy/HeaderTest.java
@@ -0,0 +1,30 @@
+package Academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import variables.Constants;
+
+
+public class HeaderTest {
+
+	
+	@Test
+	public void getData() throws InterruptedException {
+		System.out.println("Hello Guys");
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver =new ChromeDriver();
+		driver.get(Constants.USER_BASE_URL);
+		String text =driver.findElement(By.cssSelector("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase(Constants.HEADER_TEXT));
+		driver.close();
+	
+		
+		
+	}
+}
